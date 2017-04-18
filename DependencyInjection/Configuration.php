43baad769ed77166dda4d5204a229410a2018922
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-04-18 13:23:29 +0200

    Update extension configuration with db_dir global parameter

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,9 +20,13 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('q_pass_db');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+
+            ->scalarNode('db_dir')
+            ->defaultValue('~/.qmessaging_storage')
+            ->end()
+        ;
 
         return $treeBuilder;
     }
--- DependencyInjection/QPassDbExtension.php
+++ DependencyInjection/QPassDbExtension.php
@@ -22,7 +22,49 @@ class QPassDbExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $db_dir = $this->format_path(
+            $config['db_dir'],
+            $container->getParameter('kernel.root_dir')
+        );
+
+        $container->setParameter('q_pass_db.db_dir', $db_dir);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * Format a given path to absolute one
+     *  - For example, will transform ~/Documents/.gnupg as /home/user/Documents/.gnupg
+     *
+     * @param string $path
+     * @param string $rootDir
+     *
+     * @return string
+     */
+    private function format_path($path, $rootDir)
+    {
+        // retrieving current user
+        $user  = exec('whoami');
+
+        // building replacement pattern
+        $rep_1     = sprintf('/home/%s${1}', $user);
+        $rep_slash = '${1}'; // used to remove the / character at the end (eventually)
+        $rep_2     = sprintf('%s/../${1}', $rootDir);
+
+        // building regex pattern
+        $pat_1     = '/^\~(.*)\/{0,1}/';
+        $pat_slash = '/(.*)\/$/';
+        $pat_2     = '/(^[A-Za-z].*)/';
+
+        $formatted = preg_replace($pat_slash, $rep_slash, $path);
+
+        $formatted = preg_replace($pat_1, $rep_1, $formatted);
+        if ($formatted !== $path) return $formatted;
+
+        $formatted = preg_replace($pat_2, $rep_2, $formatted);
+        if ($formatted !== $path) return $formatted;
+
+        return $formatted;
     }
 }
