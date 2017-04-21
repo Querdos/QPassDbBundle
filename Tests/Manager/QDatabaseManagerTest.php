b6790ef68374d61ea4f31ce1927b4a121ab50127
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-04-21 11:51:15 +0200

    Update the QDatabaseManager test with the entity creation

--- Tests/Manager/QDatabaseManagerTest.php
+++ Tests/Manager/QDatabaseManagerTest.php
@@ -5,6 +5,7 @@ namespace Manager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\SchemaTool;
 use Querdos\QPassDbBundle\Entity\QDatabase;
+use Querdos\QPassDbBundle\Manager\QDatabaseManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Config\Definition\Exception\Exception;
 
@@ -20,6 +21,11 @@ class QDatabaseManagerTest extends KernelTestCase
      */
     private $entityManager;
 
+    /**
+     * @var QDatabaseManager
+     */
+    private $qdatabaseManager;
+
     public function setUp()
     {
         self::bootKernel();
@@ -27,6 +33,10 @@ class QDatabaseManagerTest extends KernelTestCase
         $this->entityManager = static::$kernel->getContainer()
             ->get('doctrine')
             ->getManager()
+        ;
+
+        $this->qdatabaseManager = static::$kernel->getContainer()
+            ->get('qpdb.manager.qdatabase')
         ;
 
         $metadata = $this->entityManager->getMetadataFactory()->getAllMetadata();
@@ -39,10 +49,35 @@ class QDatabaseManagerTest extends KernelTestCase
 
     public function testCreateQDatabase()
     {
-        //
+        // creating a new entity
+        $db_name   = "database_test";
+        $db_pass   = "database_test_password";
+        $qdatabase = new QDatabase($db_name, $db_pass);
+
+        // calling the manager
+        $this->qdatabaseManager->create($qdatabase);
+
+        // building a request to check if the entity has been persisted
+        $result = $this->entityManager
+            ->createQueryBuilder()
+
+            ->select("qdatabase")
+            ->from("QPassDbBundle:QDatabase", "qdatabase")
+
+            ->where("qdatabase.dbname = :db_name")
+            ->setParameter('db_name', $db_name)
+
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+
+        // asserting not null
+        $this->assertNotNull($result);
+        // asserting that the retrieved password isn't plain
+        $this->assertNotEquals($db_pass, $result->getPassword());
     }
 
-    public function tearDown()
+    protected function tearDown()
     {
         parent::tearDown();
 
